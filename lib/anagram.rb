cfb0c462c77ebccce97e51653d58e33a95206e39
Samantha Ostrowski <isphinxs@hotmail.com>
2020-11-23 21:20:07 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+
+class Anagram
+    
+    def initialize(word)
+        @word = word
+    end 
+
+    attr_accessor :word
+
+    def match(possible_anagrams)
+        sorted_word = word.split("").sort
+        possible_anagrams.select do |anagram|
+            anagram.split("").sort == sorted_word
+        end 
+    end 
+
+end
